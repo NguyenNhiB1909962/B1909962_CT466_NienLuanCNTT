6e4a5baf9421af024394ccaf7f4e2a706e00471b
NguyenNhiB1909962 <nguyennhi325380@gmail.com>
2023-03-18 21:59:19 +0700

    Trang lien he

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Liên Hệ</title>
+
+    <link href="public/frontend/css/bootstrap.min.css" rel="stylesheet">
+    <link href="public/frontend/css/font-awesome.min.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
+
+</head>
+<body>
+    <header>
+        <div class="p-3">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-4">
+						<div>
+							<a href="#"><img src="public/frontend/images/logo.jpg" alt="" /></a>
+						</div>
+					</div>
+					<div class="col-sm-8">
+                        <div style="font-size: 20px">
+							<ul class="nav justify-content-end">
+								<li><a href="cart" class="text-dark"><i class="fa fa-shopping-cart"></i> Cart</a></li>
+								<li><a href="login" class="text-dark"><i class="fa fa-sign-in" aria-hidden="true"></i> Login</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+        <div class="container">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <div class="collapse navbar-collapse" style="font-size: 20px" id="navbarSupportedContent">
+                  <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="welcome">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    &nbsp;
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Shop Products
+                        </a>
+                        <div class="dropdown-menu" style="font-size: 25px; background-color: FloralWhite" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="#">Products</a>
+                            <a class="dropdown-item" href="#">Product Details</a>
+                        </div>
+                    </li>
+                    &nbsp;
+                    <li class="nav-item">
+                        <a class="nav-link" href="contact">Contact</a>
+                    </li>
+                    </ul>
+                    <form class="form-inline my-2 my-lg-0">
+                        <input class="form-control mr-sm-2" style="height: 30px; font-size: 15px" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-success pb-4" style="height: 30px; font-size: 16px" type="submit"><i class="fa fa-search"></i></button>
+                    </form>
+                </div>
+            </nav>
+        </div>
+    </header>
+
+    <div class="container text-center pt-5">
+        <h2 style="color:tomato"><b>Cảm ơn quý khách đã ủng hộ Thế Giới Laptop</b></h2>
+        <h3>Mọi ý kiến đóng góp quý khách vui lòng gửi về email: thegioilaptop@gmail.com</h3>
+    </div>
+    <br>
+
+    <div class="container" style="padding-left: 7cm">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15715.385519487107!2d105.76282732653009!3d10.029532835293777!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31a088233c51a4f3%3A0xd782e75d776337f3!2zWHXDom4gS2jDoW5oLCBOaW5oIEtp4buBdSwgQ-G6p24gVGjGoSwgVmnhu4d0IE5hbQ!5e0!3m2!1svi!2s!4v1676453304656!5m2!1svi!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
+    
+    <div class="container pt-5" style="padding-left: 7cm; padding-right: 7cm">
+        <div class="text-center border rounded py-3" style="background-color: antiquewhite">
+            <h1><b>Thông tin liên hệ</b></h1>
+            <hr>
+            <p style="font-size: 20px"><b>Thời gian mở cửa:</b> 8:00-22:00 các ngày trong tuần</p>
+            <p style="font-size: 20px"><b>Địa chỉ:</b> Phường Xuân Khánh, quận Ninh Kiều, thành phố Cần Thơ</p>
+            <p style="font-size: 20px"><b>Facebook:</b> Thế Giới Laptop</p>
+            <p style="font-size: 20px"><b>Zalo:</b> +18 00 70 61</p>
+        </div>
+    </div>
+    <br>
+
+    <footer>
+        <div class="container">
+			<div style="font-size: 20px; background-color: lightgrey; text-align: center">
+				<a href="#"><i class="fa fa-phone"></i> +18 00 70 61</a>
+                <br>
+                <a href="#"><i class="fa fa-envelope"></i> thegioilaptop@gmail.com</a>
+                <br>
+				<a href="#"><i class="fa fa-facebook"></i></a>
+				<a href="#"><i class="fa fa-twitter"></i></a>
+				<a href="#"><i class="fa fa-linkedin"></i></a>
+			</div>
+		</div>
+    </footer>
+</body>
+</html>
